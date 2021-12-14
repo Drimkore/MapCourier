620c9f50d007a0e66f5294f3698b9978bb3c1da6
NotRude <vsleontyev@gmail.com>
2021-12-14 18:43:41 +0500

    Алгорим версии 2.0, изменения в нейминге класса и изменение классов, связанных с точками

--- MapCourier/MapCourier/DistanceFinder.cs
+++ MapCourier/MapCourier/DistanceFinder.cs
@@ -1,37 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 
 namespace MapCourier
-{
-    public class Mark
-    {
-        public readonly string X;
-        public readonly string Y;
-        public Mark NearStorage;
-        public int NearStorageDist;
-        public readonly char Status = 'n'; // 'b' - busy, 'f' - free, 'n' - not needed
-        public Mark(string x, string y)
-        {
-            X = x;
-            Y = y;
-        }
-    }
-    class MarksNDistance
-    {
-        public readonly Mark Mark1;
-        public readonly Mark Mark2;
-        public readonly int Distance;
-        public MarksNDistance(Mark mark1, Mark mark2)//При объявлении конструкор сам считает дистанцию по точкам. По моему, это будет удобнее чем создавать отдельный метод.
-        {
-            Mark1 = mark1;
-            Mark2 = mark2;
-            Distance = Convert.ToInt32(FindDistance.GetDistance(mark1.X, mark1.Y, mark2.X, mark2.Y));
-        }
-    }
+{   
     //Думаю классы понятны и просты, если что-то не так, сами меняйте я не хочу 
-    class FindDistance
+    class DistanceFinder
     {
         public static string GetDistance(string x1, string y1, string x2, string y2)//Возвращает дистанцию по координатам 2-х точек.
         {
@@ -72,7 +48,21 @@ namespace MapCourier
             result = result.Remove(result.IndexOf(','));
             return result;
         }
+        public static double GetMapsDistance(Mark mark1, Mark mark2)
+        {
+            var mark1X = Convert.ToDouble(mark1.X);
+            var mark1Y = Convert.ToDouble(mark1.Y);
+            var mark2X = Convert.ToDouble(mark2.X);
+            var mark2Y = Convert.ToDouble(mark2.Y);
+            var lengthX = mark1X - mark2X;
+            var lengthY = mark1Y - mark2Y;
+            return Math.Sqrt(lengthX * lengthX + lengthY * lengthY);
+        }
     }
+
+
+
+
     /// <summary>
     /// Граф
     /// </summary>
--- MapCourier/MapCourier/PathFinder.cs
+++ MapCourier/MapCourier/PathFinder.cs
@@ -1,72 +1,220 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MapCourier
 {
-    public class PathFinder
+    class Mark : IComparable, IComparer<List<Mark>>
     {
-        List<Mark> ClientMarks = new List<Mark>();
-        List<Mark> StorageMarks = new List<Mark>();
-        private static List<Mark> GetTestMarks(int count)
+        public readonly string X;
+        public readonly string Y;
+        public int ID;
+        public List<Mark> PastMark = new List<Mark>();
+        public double NearStorageDist;
+        public double PastMarkDist;
+        public List<Mark> NearMarks = new List<Mark>();
+        public double NearMarkDist;
+        public int IterationCount = 0;
+        public readonly char Status = 'n'; // 'b' - busy, 'f' - free, 'n' - not needed
+        public Mark(string x, string y)
         {
-            var test = new List<Mark>();
-            Random random = new Random();
-            for(var i = 0; i < count; i++)
-            {
-                random.Next(1, 100);
-                var x = random.ToString();
-                random.Next(1, 100);
-                var y = random.ToString();
-                test.Add(new Mark(x, y));
-            }
-            return test;
+            X = x;
+            Y = y;
         }
-        public List<Mark> FindPath(double maxRad)
+        public Mark()
         {
-            var starMark = new Mark("50", "50");
-            double x = Convert.ToDouble(starMark.X);
-            double y = Convert.ToDouble(starMark.Y);
-            var path = new List<Mark>();
-            var nearMarks = new List<Mark>();
 
-            for (double r = 1; r < maxRad && nearMarks.Count <= 3; r++)
+        }
+        public Mark(Mark mark)
+        {
+            PastMarkDist = mark.PastMarkDist;
+            ID = mark.ID;
+            X = mark.X;
+            Y = mark.Y;
+            PastMark = new List<Mark>(mark.PastMark);
+            NearStorageDist = mark.NearStorageDist;
+            NearMarks = new List<Mark>(mark.NearMarks);
+            NearMarkDist = mark.NearMarkDist;
+            IterationCount = mark.IterationCount;
+            Status = mark.Status;
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            try
             {
-                nearMarks.Clear();
-                foreach (var i in ClientMarks)
+                var mark = (Mark)obj;
+            }
+            catch
+            {
+                throw new ArgumentException("Object is not a Temperature");
+            }
+            Mark otherMark = obj as Mark;
+            if (otherMark != null)
+            {
+                if (this.ID == otherMark.ID)
+                    return 0;
+                if (this.ID < otherMark.ID)
+                    return 1;
+                if (this.ID == otherMark.ID)
+                    return -1;
+            }
+            return 1;
+        }
+
+
+        public int Compare(List<Mark> x, List<Mark> y)
+        {
+            var a = 0;
+            if (x[x.Count - 1].ID == y[y.Count - 1].ID)
+            {
+                if (x.Count < y.Count)
+                    a--;
+                if (x.Count > y.Count)
+                    a++;
+            }
+            if (x[x.Count - 1].ID + a < y[y.Count - 1].ID)
+                return 1;
+            if (x[x.Count - 1].ID + a > y[y.Count - 1].ID)
+                return -1;
+            else
+                return 0;
+        }
+    }
+    class MarksNDistance : IComparer
+    {
+        public readonly Mark Mark1;
+        public readonly Mark Mark2;
+        public readonly double Distance;
+        public MarksNDistance(Mark mark1, Mark mark2)
+        { 
+            var 
+            Mark1 = mark1;
+            Mark2 = mark2;
+            Distance = Convert.ToDouble(DistanceFinder.GetDistance(mark1.X, mark1.Y, mark2.X, mark2.Y));
+        }
+
+        public int Compare(object x, object y)
+        {
+            MarksNDistance d1 = (MarksNDistance)x;
+            MarksNDistance d2 = (MarksNDistance)y;
+            if (d1.Distance < d2.Distance)
+                return 1;
+            if (d1.Distance > d2.Distance)
+                return -1;
+            else
+                return 0;
+        }
+    }
+    class PathFinder
+    {
+        private static double Spread = 100;
+        public static List<Mark> Result = new List<Mark>();
+        public static void FindPath(Mark mark)
+        {
+            if (mark.PastMark.Count > mark.IterationCount)
+            {
+                return;
+            }
+            if (mark.PastMark.Count > 2)
+            {
+                Result.Add(new Mark(mark));
+                return;
+            }
+
+            mark.NearMarkDist = double.MaxValue;
+            foreach (var i in ClientMarks)
+            {
+                Mark ClientMark = new(i);
+                double distance = DistanceFinder.GetMapsDistance(mark, ClientMark);
+                if (distance == 0)
+                    continue;
+                if (mark.PastMark.Count > 1 && mark.PastMark[mark.PastMark.Count - 1].NearMarkDist > distance + Spread)
+                    continue;
+                bool flag = false;
+                foreach (var j in mark.PastMark)
                 {
-                    var markX = Convert.ToDouble(i.X);
-                    var markY = Convert.ToDouble(i.Y);
-                    var lengthX = markX - x;
-                    var lengthY = markY - y;
-                    if (Math.Sqrt(lengthX * lengthX + lengthY * lengthY) <= r)
-                        nearMarks.Add(i);
+                    if (j.ID == ClientMark.ID)
+                        flag = true;
+                }
+                if (flag)
+                    continue;
+                if (distance <= mark.NearMarkDist)
+                {
+                    if (mark.NearMarkDist - distance < Spread)
+                    {
+                        for (var j = 0; j < mark.NearMarks.Count; j++)
+                        {
+                            if (mark.NearMarks[j].PastMarkDist > distance + Spread)
+                                mark.NearMarks.Remove(mark.NearMarks[j]);
+                        }
+                    }
+                    ClientMark.PastMarkDist = distance;
+                    ClientMark.PastMark = new List<Mark>(mark.PastMark);
+                    ClientMark.PastMark.Add(new(mark));
+                    ClientMark.IterationCount = mark.IterationCount + 1;
+                    mark.NearMarks.Add(ClientMark);
+                    mark.NearMarkDist = distance;
                 }
             }
-            foreach (var i in nearMarks)
+            for (var i = 0; i < mark.NearMarks.Count; i++)
             {
-                var markX = Convert.ToDouble(i.X);
-                var markY = Convert.ToDouble(i.Y);
-                var nearStorageDist = double.MaxValue;
+                FindPath(mark.NearMarks[i]);
+            }
+            if (mark.NearMarks.Count == 0 && mark.PastMark.Count > 0)
+            {
+                Result.Add(new(mark));
+            }
+        }
+
+        public static List<List<Mark>> GetAllPath(List<Mark> marks)
+        {
+            List<List<Mark>> result = new List<List<Mark>>();
+            for (var i = 0; i < marks.Count; i++)
+            {
+                var m = marks[i];
+                List<Mark> resultMarks = new List<Mark>(m.PastMark);
+                resultMarks.Add(m);
+                m.NearStorageDist = double.MaxValue;
+                Mark nearStorage = null;
                 foreach (var s in StorageMarks)
                 {
-                    var storageX = Convert.ToDouble(s.X);
-                    var storageY = Convert.ToDouble(s.Y);
-                    var lengthX = markX - storageX;
-                    var lengthY = markY - storageY;
-                    var distance = Math.Sqrt(lengthX * lengthX + lengthY * lengthY);
-                    if (distance < nearStorageDist)
+                    var distance = DistanceFinder.GetMapsDistance(m, s);
+                    if (distance <= m.NearStorageDist)
                     {
-                        nearStorageDist = distance;
-                        i.NearStorage = s;
+                        nearStorage = s;
+                        m.NearStorageDist = distance;
                     }
                 }
-                i.NearStorageDist = Convert.ToInt32(FindDistance.GetDistance(i.X, i.Y, i.NearStorage.X, i.NearStorage.Y));
+                resultMarks.Add(nearStorage);
+                result.Add(resultMarks);
             }
-
-            //Нужна графы, нихачу графы, попозже графы
-            return path;
+            return result;
         }
+
+        public static List<List<Mark>> GetBestPaths(List<List<Mark>> marks, int count)
+        {
+            marks.Sort(new Mark());
+            List<List<Mark>> result = new List<List<Mark>>();
+            SortedDictionary<double, List<Mark>> keyValuePairs = new SortedDictionary<double, List<Mark>>();
+            foreach (var i in marks)
+            {
+                double c = 0;
+                foreach (var j in i)
+                {
+                    c += j.PastMarkDist + j.NearStorageDist;
+                }
+                keyValuePairs.Add(c, i);
+            }
+            for (var i = 0; i < count; i++)
+            {
+                result.Add(keyValuePairs.ElementAt(i).Value);
+            }
+            return result;
+        }
+
+        
     }
 }
