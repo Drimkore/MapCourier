b53ea12bcef18675c62c11a84bb3667f7415edaa
Svenan <egorc42@gmail.com>
2022-02-15 19:00:09 +0500

    404 (В комментах HomeController)

--- MapCourier/MapCourier/Controllers/HomeController.cs
+++ MapCourier/MapCourier/Controllers/HomeController.cs
@@ -15,6 +15,9 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        //if (User.Identity.IsAuthenticated) { return View(); }  РУКАМИ НЕ ТРОГАТЬ
+        //else { return NotFound();
+        //}
         return View();
     }
 
