cfeec54b7252c2cea11eaac742b06537d7c89823
Drimkore <art.2000m@mail.ru>
2022-05-27 20:06:07 +0500

    Добавил в реге ввод имени пользователя, Логин идет по userName а не по email.

--- MapCourier/MapCourier/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MapCourier/MapCourier/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -82,6 +82,10 @@ namespace MapCourier.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
+            [Required]
+            [Display(Name = "UserName")]
+            public string UserName { get; set;}
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -117,8 +121,8 @@ namespace MapCourier.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            var userByRole = await _userManager.GetUsersInRoleAsync("Admin");            
-            if ((!User.Identity.IsAuthenticated && userByRole.Count()>0)||(!User.IsInRole("Admin") && User.Identity.IsAuthenticated)) 
+            var userByRoleAdmin = await _userManager.GetUsersInRoleAsync("Admin");            
+            if ((!User.Identity.IsAuthenticated && userByRoleAdmin.Count()>0)||(!User.IsInRole("Admin") && User.Identity.IsAuthenticated)) 
             {
                 return NotFound();
             }
@@ -130,7 +134,7 @@ namespace MapCourier.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
