9b7dbd6aed90749a1dc9b83b09d89b005f9519f4
Drimkore <art.2000m@mail.ru>
2022-06-07 18:14:14 +0500

    Теперь время хранит часы и минуты

--- MapCourier/MapCourier/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ MapCourier/MapCourier/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -110,7 +110,8 @@ namespace MapCourier.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, 
+                                                                      Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
